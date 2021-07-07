a92ee3aaa9f1af0867236b8c9f167d332ca8dbe6
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2021-07-07 10:27:45 -0300

    build: add pip no-binary configuration and fixes
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

--- charmcraft/parts.py
+++ charmcraft/parts.py
@@ -32,6 +32,7 @@ class CharmPluginProperties(plugins.PluginProperties, plugins.PluginModel):
     source: str = ""
     charm_requirements: List[str] = []
     charm_python_packages: List[str] = []
+    charm_allow_pip_binary: bool = False
 
     @classmethod
     def unmarshal(cls, data: Dict[str, Any]):
@@ -58,19 +59,19 @@ class CharmPlugin(plugins.Plugin):
     def get_build_environment(self):
         """Return a dictionary with the environment to use in the build step."""
         plugin_venv_dir = self._part_info.work_dir / "venv"
-        venv_dir = self._part_info.part_install_dir / self._part_info.venv_dir
+        charm_venv_dir = self._part_info.part_install_dir / self._part_info.venv_dir
         return {
             # Add PATH to the python interpreter we always intend to use with
             # this plugin. It can be user overridden, but that is an explicit
             # choice made by a user.
-            "PATH": "{}/bin:{}/bin:${{PATH}}".format(str(venv_dir), str(plugin_venv_dir)),
+            "PATH": "{}/bin:{}/bin:${{PATH}}".format(str(charm_venv_dir), str(plugin_venv_dir)),
         }
 
     def get_build_commands(self):
         """Return a list of commands to run during the build step."""
         plugin_venv_dir = self._part_info.work_dir / "venv"
-        venv_dir = self._part_info.part_install_dir / self._part_info.venv_dir
-        pip_install_cmd = f"pip3 install --target={venv_dir} --no-binary :all:"
+        charm_venv_dir = self._part_info.part_install_dir / self._part_info.venv_dir
+        pip_install_cmd = f"pip install --target={charm_venv_dir}"
         commands = [
             'python3 -m venv "{}"'.format(plugin_venv_dir),
             "pip install -U pip setuptools wheel",
@@ -78,16 +79,19 @@ class CharmPlugin(plugins.Plugin):
 
         options = cast(CharmPluginProperties, self._options)
 
+        if not options.charm_allow_pip_binary:
+            pip_install_cmd += " --no-binary :all:"
+
         if options.charm_python_packages:
             python_packages = " ".join(
                 [shlex.quote(pkg) for pkg in options.charm_python_packages]
             )
-            python_packages_cmd = f"{pip_install_cmd} -U {python_packages}"
+            python_packages_cmd = f"{pip_install_cmd} {python_packages}"
             commands.append(python_packages_cmd)
 
         if options.charm_requirements:
             requirements = " ".join(f"-r {r!r}" for r in options.charm_requirements)
-            requirements_cmd = f"{pip_install_cmd} -U {requirements}"
+            requirements_cmd = f"{pip_install_cmd} {requirements}"
             commands.append(requirements_cmd)
 
         install_dir = self._part_info.part_install_dir
